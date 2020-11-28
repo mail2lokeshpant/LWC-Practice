a6a677cc670bdda88047c11590870be28f328974
Pant <772597@cognizant.com>
2020-11-28 12:26:39 +1100

    Lead changes

--- .sfdx/typings/lwc/apex/AccountController.d.ts
+++ .sfdx/typings/lwc/apex/AccountController.d.ts
@@ -1,3 +1,12 @@
 declare module "@salesforce/apex/AccountController.fetchAccounts" {
   export default function fetchAccounts(): Promise<any>;
 }
+declare module "@salesforce/apex/AccountController.fetchContacts" {
+  export default function fetchContacts(): Promise<any>;
+}
+declare module "@salesforce/apex/AccountController.fetchOpportunity" {
+  export default function fetchOpportunity(): Promise<any>;
+}
+declare module "@salesforce/apex/AccountController.fetchLead" {
+  export default function fetchLead(): Promise<any>;
+}
